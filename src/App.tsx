695b5ece86ac305af7eaa5b01058b62715e887e2
harshana-lk <harshuonlive@gmail.com>
2025-01-08 12:27:50 +0530

    initialize project with Vite, React, and TypeScript setup; add basic components and configuration files

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
+function App() {
+  // useBreakpointValue
+  const showSidebar = useBreakpointValue({ base: false, lg: true });
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" "footer"`,
+        lg: `"nav nav" "aside main" "footer footer"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+
+      {/* Using useBreakpointValue */}
+      {showSidebar && (
+        <GridItem area="aside" bg={"red"}>
+          Side Bar
+        </GridItem>
+      )}
+      <GridItem area="main" bg={"green"}>
+        Main Content
+      </GridItem>
+      <GridItem area="footer" bg={"brown"}>
+        Footer
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
